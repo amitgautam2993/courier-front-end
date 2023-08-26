8261cfe2440a6f8607b3dd54044988f5cabe8d4d
amitgautam2993 <amitgautam2993@gmail.com>
2023-08-26 23:06:21 +0530

    Environment Bug Fixed where app will detect env and change it's URL accordingly

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://192.168.0.198/track4uBackend'
+    production: false,
+    apiUrl: 'http://localhost:9002'
   };
-  
 
     
